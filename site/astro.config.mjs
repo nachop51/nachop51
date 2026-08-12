03e0a5298317db9e30c69265b165f7c9f530c2d2
nachop51 <nachoperalta0@gmail.com>
2026-08-12 10:41:23 +0400

    void astro project

--- site/astro.config.mjs
+++ site/astro.config.mjs
@@ -1,5 +1,40 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import cloudflare from "@astrojs/cloudflare";
+import { voidPlugin } from "void";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import { defineConfig, fontProviders } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://nachop.dev",
+  integrations: [mdx(), sitemap()],
+  adapter: cloudflare(),
+  vite: {
+    plugins: [voidPlugin()],
+  },
+  fonts: [
+    {
+      provider: fontProviders.local(),
+      name: "Atkinson",
+      cssVariable: "--font-atkinson",
+      fallbacks: ["sans-serif"],
+      options: {
+        variants: [
+          {
+            src: ["./src/assets/fonts/atkinson-regular.woff"],
+            weight: 400,
+            style: "normal",
+            display: "swap",
+          },
+          {
+            src: ["./src/assets/fonts/atkinson-bold.woff"],
+            weight: 700,
+            style: "normal",
+            display: "swap",
+          },
+        ],
+      },
+    },
+  ],
+});
